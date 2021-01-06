355e33502e139f07397a99a5bd08c53f74873453
JanUlrich <andi@michlaustderaffe.de>
2021-01-06 22:56:10 +0100

    Add Bell sound

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,98 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(CountdownTimer());
+
+class RingTimerApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class CountdownTimer extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  CountdownTimerState createState() {
+    return CountdownTimerState(5);
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class CountdownTimerState extends State<StatefulWidget> {
+  static AudioCache player = AudioCache();
+  int duration = 60;
+  DateTime start;
+  var timeString = "00:00";
+  Timer timer = null;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  CountdownTimerState(int duration){
+    this.duration = duration;
+  }
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+  void initState() {
+    start = DateTime.now();
+    timeString = "03:00";
+    timer = Timer.periodic(Duration(seconds: 1), (Timer t) => _updateTimer(t));
+    super.initState();
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  @override
+  Widget build(BuildContext context) {
 
-  void _incrementCounter() {
+    return MaterialApp(
+      title: 'Ring Timer',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Timer'),
+        ),
+        body: Center(
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(timeString,
+            style: new TextStyle(
+                fontSize: 80.0
+              ))
+                 // ,style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _updateTimer(Timer t) {
+    Duration sinceStart = DateTime.now().difference(start);
+    int remaining = duration - sinceStart.inSeconds;
+    /*
+    Trying to play sounds with:
+    https://github.com/luanpotter/audioplayers/blob/master/doc/audio_cache.md
+     */
+    if(remaining <= 0)player.play("bell.opus");
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      timeString = formatSeconds(remaining);
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  String formatSeconds(int s){
+    if(s < 0)return "00:00";
+    return ((s/600).floor()%10).toString() + ((s/60).floor()%10).toString()
+        + ":" + ((s/10).floor()%10).toString() + ((s).floor()%10).toString();
   }
+
+  /*
+  @override
+  void dispose() {
+    if(timer != null) timer.cancel();
+    super.dispose();
+  }
+   */
 }
